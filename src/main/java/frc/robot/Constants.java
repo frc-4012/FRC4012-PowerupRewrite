28fe28a6fcf2a1e246b28020364a84a79471be57
Daniel Pearson <dpear0915@gmail.com>
2019-11-22 10:45:40 -0500

    Create Constants.java

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+public class Constants{
+  public static final int
+  //Drive Train
+    LEFT_FRONT = 0, 
+    LEFT_BACK = 1,
+    RIGHT_FRONT = 2,
+    RIGHT_BACK = 3;
+    
+  
+  public static final int LEFT_JOY = 0;
+  public static final int RIGHT_JOY = 1;
+  
+  
+    
+    
+    
+}
